d4fec95b265662ff851f53e2a3e5df6bcb236d93
hemann6 <hemann.6@buckeyemail.osu.edu>
2022-02-16 14:36:45 -0500

    Added pre-game countdown for classic mode. Added in-game messages when a selected SET is correct or incorrect.

--- lab-1/js/board/classicMode.js
+++ lab-1/js/board/classicMode.js
@@ -1,4 +1,22 @@
 import { generateBoard, createAddCardButton } from './boardFunctions.js';
 
-generateBoard(12);
-createAddCardButton();
+    //Displays a countdown from 3 seconds, then starts the game
+    let t = 3;
+    let countDown = setInterval(gameStart, 1000);
+    document.getElementById("mesgTop").innerHTML = "Get Ready!"; //@GameFlow
+    function gameStart(){
+        //updates countdown until game starts
+        if(t>0){
+            document.getElementById("mesgBottom").innerHTML = "Game starting in: " + t; //@GameFlow
+            t--;
+        }    
+        //At the end of the countdown, the game functions start and the interval timer stops
+        else{
+            document.getElementById("mesgTop").innerHTML = "Cards Dealt"; //@GameFlow
+            document.getElementById("mesgBottom").innerHTML = "Look for a SET!"; //@GameFlow  
+            clearInterval(countDown);
+            generateBoard(12);
+            createAddCardButton();
+        }
+        
+    }
