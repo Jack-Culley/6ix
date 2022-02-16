370aca7cf739704cc774c6f362a1716950e9fcf8
hemann6 <hemann.6@buckeyemail.osu.edu>
2022-02-16 15:54:14 -0500

    Hide 'add card' button before game starts

--- lab-1/js/board/classicMode.js
+++ lab-1/js/board/classicMode.js
@@ -3,6 +3,7 @@ import { generateBoard, createAddCardButton } from './boardFunctions.js';
     //Displays a countdown from 3 seconds, then starts the game
     let t = 3;
     let countDown = setInterval(gameStart, 1000);
+    document.getElementById("add-card-button").style.display = 'none'; //@GameFlow
     document.getElementById("mesgTop").innerHTML = "Get Ready!"; //@GameFlow
     function gameStart(){
         //updates countdown until game starts
@@ -15,6 +16,7 @@ import { generateBoard, createAddCardButton } from './boardFunctions.js';
             document.getElementById("mesgTop").innerHTML = "Cards Dealt"; //@GameFlow
             document.getElementById("mesgBottom").innerHTML = "Look for a SET!"; //@GameFlow  
             clearInterval(countDown);
+            document.getElementById("add-card-button").style.display = 'block'; //@GameFlow
             generateBoard(12);
             createAddCardButton();
         }
