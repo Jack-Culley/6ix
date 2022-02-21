8abc66deae8a2816426dc8a27fd23174088c0461
jison06 <ison.84@buckeyemail.osu.edu>
2022-02-20 22:27:23 -0500

    Fixing classic mode

--- lab-1/js/board/boardFunctions.js
+++ lab-1/js/board/boardFunctions.js
@@ -1,10 +1,14 @@
 "use strict"
 import { Card, generateCards, getRandomInt } from '../cards/cardObjects.js';
-import { speed } from '../board/speedMode.js';
 let clickedCards = new Map();
 let cards = [];
 let board = [];
 let usedCards = [];
+let speedModeStats = {
+  wins: 0,
+  loses: 0,
+  time: 0,
+}
 
 function highlightCard(isClicked, cardElement, cardObject) {
   if(isClicked) {
@@ -77,6 +81,34 @@ function addToEmptySpace() {
   return columnArray.filter((column) => column.children.length < 4)[0];
 }
 
+//Displays a game message for 1 second based on validity of chosen cards. @GameFlow
+function displaySetMessage(isSet) {
+  let className = isSet ? "is-success" : "is-danger";
+  let messageTop = isSet ? "SET Won!" : "Not a SET!";
+  let messageBottom;
+  if(document.getElementById("speed")?.firstChild.nodeValue.includes("Speed")){
+    if(!isSet){
+      messageBottom = '+5 seconds';
+      speedModeStats.loses += 1;
+      speedModeStats.time += 1;
+    } else {
+    speedModeStats.wins += 1;
+    messageBottom = "<br>";
+    }
+  } else {
+    messageBottom = isSet ? "+1 Point" : "-1 Point";
+  }
+  document.getElementById("game-message").classList.toggle(className);
+  document.getElementById("mesg-top").innerHTML = messageTop;
+  document.getElementById("mesg-bottom").innerHTML = messageBottom;
+  setTimeout(function(){
+    if(board.length == 0) return;
+    document.getElementById("game-message").classList.toggle(className);
+    document.getElementById("mesg-top").innerHTML = "<br>";
+    document.getElementById("mesg-bottom").innerHTML = "Look for a SET!";
+  }, 1000)
+}
+
 //Button click event listener that handles set checking and card highlighting
 function buttonClick(click) {
   let target = click.target;
@@ -88,37 +120,9 @@ function buttonClick(click) {
     clickedCards.set(card, target)
   }
 
-  //Displays a game message for 1 second based on validity of chosen cards. @GameFlow
-  function displaySetMessage(isSet) {
-    let className = isSet ? "is-success" : "is-danger";
-    let messageTop = isSet ? "SET Won!" : "Not a SET!";
-    let messageBottom;
-    if(document.getElementById("speed").firstChild.nodeValue.includes("Speed")){
-      if(!isSet){
-        messageBottom = '+5 seconds';
-        speed.loses += 1;
-        speed.time += 1;
-      } else {
-      speed.wins += 1;
-      messageBottom = "<br>";
-      }
-    } else {
-      messageBottom = isSet ? "+1 Point" : "-1 Point";
-    }
-    document.getElementById("game-message").classList.toggle(className);
-    document.getElementById("mesg-top").innerHTML = messageTop; 
-    document.getElementById("mesg-bottom").innerHTML = messageBottom; 
-    setTimeout(function(){
-      if(board.length == 0) return;
-      document.getElementById("game-message").classList.toggle(className);
-      document.getElementById("mesg-top").innerHTML = "<br>"; 
-      document.getElementById("mesg-bottom").innerHTML = "Look for a SET!"; 
-    }, 1000)
-}
-
   if(clickedCards.size >= 3) {
     displaySetMessage(checkIfSet());
-    if(!checkIfSet()) {      
+    if(!checkIfSet()) {
       clickedCards.forEach((cardElement, cardObject) => {
         cardObject.isClicked = false;
         //unhighlights a card if selected cards are not part of a set
@@ -187,4 +191,4 @@ function cleanUp() {
 }
 
 generateCards(cards);
-export { generateBoard, createAddCardButton, addCards, board, cards };
+export { generateBoard, createAddCardButton, addCards, board, cards, speedModeStats };
