2e64183d0ad2127c38a8151d27276f6658b61cd2
Josh Ison <ison.84@buckeyemail.osu.edu>
2022-03-21 19:42:07 -0400

    linting

--- lab-2/app/controllers/application_controller.rb
+++ lab-2/app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     dashboard_index_path
   end
 
--- lab-2/app/controllers/course_controller.rb
+++ lab-2/app/controllers/course_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CourseController < ApplicationController
   def edit
     @course = Course.find_by(id: params[:id])
