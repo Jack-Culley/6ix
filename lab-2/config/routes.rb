605c70f7b08547e6e659c868084c80207b405761
Drew Holowaty <drewholowaty@gmail.com>
2022-03-27 17:33:52 -0400

    added functionality for admin user to approve other admin and instructor users

--- lab-2/config/routes.rb
+++ lab-2/config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
   resources 'course'
   resources 'profile', only: %w[index]
 
+  resources 'user_approval' do
+    collection do
+      put :approve_button_click
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
