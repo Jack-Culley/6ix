0f54b394eb4400a0a50eb2ca142ff270c2645ae9
Jack-Culley <jcu2ley@gmail.com>
2022-03-27 16:05:37 -0400

    fixed route to change password

--- lab-2/app/models/course.rb
+++ lab-2/app/models/course.rb
@@ -4,4 +4,7 @@ class Course < ApplicationRecord
   has_many :courses_users
   has_many :users, through: :courses_users
   has_many :sections
+  validates :department, presence: true
+  validates :campus, presence: true
+  validates :course_title, presence: true
 end
