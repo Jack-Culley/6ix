77f55109f3a65d577587fde67e16efecde07a67f
Jack-Culley <jcu2ley@gmail.com>
2022-03-27 15:48:19 -0400

    fixed route to change password

--- lab-2/config/routes.rb
+++ lab-2/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'home#index'
   resources 'dashboard', only: %w[index]
   resources 'course'
-  get "/profile", to: "profile#index"
+  resources 'profile', only: %w[index]
 
 
 
