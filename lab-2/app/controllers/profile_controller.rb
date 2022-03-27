fe784b8bace1e230681f6b90578cdfa07cba692e
Jack-Culley <jcu2ley@gmail.com>
2022-03-27 16:42:09 -0400

    changed conventions

--- lab-2/app/controllers/profile_controller.rb
+++ lab-2/app/controllers/profile_controller.rb
@@ -2,10 +2,7 @@
 
 class ProfileController < ApplicationController
   def index
-    @first = current_user.first_name
-    @last = current_user.last_name
-    @email = current_user.email
-    @type = current_user.user_type
+    @user = current_user
   end
 
 end
