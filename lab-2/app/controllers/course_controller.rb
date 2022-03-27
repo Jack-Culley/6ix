cb90acfc69e7b3131062f83a76f37f5822f74403
Zach Katz <katz.427@osu.edu>
2022-03-26 22:43:59 -0400

    Added admin ability to add/edit/delete courses with error messages

--- lab-2/app/controllers/course_controller.rb
+++ lab-2/app/controllers/course_controller.rb
@@ -1,9 +1,46 @@
 # frozen_string_literal: true
 
 class CourseController < ApplicationController
+  respond_to :html
+
   def edit
     @course = Course.find_by(id: params[:id])
   end
 
-  def update; end
+  def update
+    @course = Course.find_by(id: params[:id])
+    @course.update(course_params)
+    if @course.valid?
+      redirect_to dashboard_index_url
+    else
+      respond_with @course
+    end
+  end
+
+  def destroy
+    course = Course.find_by(id: params[:id])
+    sections = Section.where(course_id: course.id)
+    sections.delete_all
+    course.delete
+    redirect_to dashboard_index_url
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.create(course_params)
+    if @course.valid?
+      redirect_to dashboard_index_url
+    else
+      respond_with @course
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:department, :campus, :course_title)
+  end
 end
