0860493782e614f44d81f7c0a2faad145ffcf698
Jack-Culley <jcu2ley@gmail.com>
2022-03-27 14:42:07 -0400

    Added route to page, basic user info, reset password button

--- lab-2/config/routes.rb
+++ lab-2/config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   root 'home#index'
   resources 'dashboard', only: %w[index]
   resources 'course'
+  get "/profile", to: "profile#index"
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
