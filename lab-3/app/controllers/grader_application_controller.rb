2f5e3d06de04161faf5facae95ec03d405c20e5c
Josh Ison <ison.84@buckeyemail.osu.edu>
2022-04-11 13:09:41 -0400

    Adding availability saving to form

--- lab-3/app/controllers/grader_application_controller.rb
+++ lab-3/app/controllers/grader_application_controller.rb
@@ -9,33 +9,76 @@ class GraderApplicationController < ApplicationController
   def new
     @user = current_user
     @course = Course.new
+    @availabilities = Availability.new
   end
 
   def create
     @user = current_user
     courses_taken = application_params&.dig(:courses_taken_attributes).to_h
-    courses_taken.each do |key, _value|
-      binding.pry
-      course_params = courses_taken[key]
-      course_params.delete('_destroy')
-      @course = CoursesTaken.create(course_params)
-      respond_with(@user, @course) && return unless @course.valid?
-    end
-    # regexp for restricting date \A(\([0-2]{1}\d{1,3},[0-2]{1}\d{1,3}\),{0,1})+\z
+    fill_courses_taken(courses_taken)
+    availability_params = application_params&.dig(:availability)
+    has_errors = create_availabilities(availability_params)
+
+    return if has_errors
 
     flash[:notice] = 'Successfully submitted course(s) taken'
     redirect_to dashboard_index_path
   end
 
+  private
+
   def application_params
     params.require(:user).permit(courses_taken_attributes: %i[id letter_grade department course_number _destroy],
-                                 availability: [monday: %i[is_available start_time
-                                                           end_time], tuesday: %i[is_available start_time
-                                                                                  end_time], wednesday: %i[is_available start_time
-                                                                                                           end_time], thursday: %i[
-                                                                                                             is_available start_time end_time
-                                                                                                           ], friday: %i[
-                                                                                                             is_available start_time end_time
-                                                                                                           ]])
+                                 availability: [monday: %i[is_available
+                                                           availabilities], tuesday: %i[is_available
+                                                                                        availabilities], wednesday: %i[
+                                                                                          is_available availabilities
+                                                                                        ], thursday: %i[
+                                                                                          is_available availabilities
+                                                                                        ], friday: %i[
+                                                                                          is_available availabilities
+                                                                                        ]])
+  end
+
+  def fill_courses_taken(courses_taken)
+    # TODO: Link courses taken & availabilities to a user
+    flash[:alert] = []
+    courses_taken.each do |key, _value|
+      course_params = courses_taken[key]
+      course_params.delete('_destroy')
+      @course = CoursesTaken.create(course_params)
+      flash[:alert] << @course.errors.full_messages
+    end
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize
+  def create_availabilities(availability_params)
+    regexp = /\A(\([0-2]{1}\d{1,3},[0-2]{1}\d{1,3}\),{0,1})+\z/
+    count = 0
+    @availabilities = Availability.new
+    # This block ensures that the availabilities are not empty or filled in correctly
+    availability_params.to_h.each do |day, value|
+      if value[:availabilities].empty?
+        count += 1
+        next
+      end
+
+      unless value[:availabilities].match?(regexp)
+        @availabilities.errors.add(day.to_sym,
+                                   " availabilites aren't in the correct format.")
+      end
+    end
+
+    @availabilities.errors.add(:availabilities, ' no availabilities selected') if count == 5
+    unless @availabilities.errors.empty? && flash[:alert].empty?
+      flash[:alert] << @availabilities.errors.full_messages if @availabilities.errors.any?
+      redirect_to new_grader_application_url
+      return true
+    end
+
+    json_obj = { data: availability_params }.to_json
+    @availabilities.update(availabiliy_json: json_obj, user_id: @user.id)
+    false
   end
 end
