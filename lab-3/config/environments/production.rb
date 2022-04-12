656463c9720b269acac49f0e03eeff525b9f9d3f
Drew Holowaty <drewholowaty@gmail.com>
2022-04-11 22:19:27 -0400

    updated naming from lab2 to lab3 in files

--- lab-3/config/environments/production.rb
+++ lab-3/config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "lab_2_production"
+  # config.active_job.queue_name_prefix = "lab_3_production"
 
   config.action_mailer.perform_caching = false
 
