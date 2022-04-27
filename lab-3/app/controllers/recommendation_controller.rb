023a84636fd6e42b065d10f83b72ecbea1e2ff9d
Jack-Culley <jcu2ley@gmail.com>
2022-04-27 18:04:26 -0400

    Fixed records being displayed

--- lab-3/app/controllers/recommendation_controller.rb
+++ lab-3/app/controllers/recommendation_controller.rb
@@ -15,19 +15,19 @@ class RecommendationController < ApplicationController
   end
 
   def get_instructor_name(instructor_id)
-    @first_name = User.find(id: instructor_id).first_name
-    @last_name = User.find(id: instructor_id).last_name
-    return @first_name + " " + @last_name
+    first_name = User.find(id: instructor_id).first_name
+    last_name = User.find(id: instructor_id).last_name
+    return first_name + " " + last_name
   end
   helper_method :get_instructor_name
 
   def get_recommendation_status(user_id)
-    @student = User.find(user_id)
-    if @student.courses_taken.empty?
+    student = User.find(user_id)
+    if student.courses_taken.empty?
       return "Not Recommended"
     end
-    @courses = @student.courses_taken
-    @courses.all.each do |c|
+    courses = student.courses_taken
+    courses.all.each do |c|
         if c.is_recommended
           return "Recommended"
           exit
